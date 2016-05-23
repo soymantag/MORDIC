5f8b4573cfc74b2b3b026db9647151143e3c59b5
chris <soymantag@163.com>
2016-05-23 12:34:08 +0800

    utils

---
+++ app/src/main/java/com/chris/mordic/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.chris.mordic.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Email: soymantag@163.coom
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
